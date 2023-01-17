2d0e473478469dc952debb8cc57b7359f1b0b9ea
gui-ying233 <zhuyufei20050307@gmail.com>
2023-01-16 21:51:57 -0500

    Update

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import os
 import json
+from time import sleep
 
-newJSON = {}
+while 1:
+    newJSON = {}
 
-for _ in os.listdir("/Users/guiying/Downloads"):
-    if _[:4] == "萌娘百科" and _ [-15:] == "flowthread.json":
-        try:
-            print(_)
-            os.rename("/Users/guiying/Downloads/" + _,
-                      "./Lih萌百镜像站flowthread-原.json")
-            for _ in json.load(open("Lih萌百镜像站flowthread-原.json")):
-                newJSON[_["title"]] = _["posts"]
-            open("Lih萌百镜像站flowthread.json", "w").write(
-                json.dumps(newJSON, indent=4))
-        finally:
+    for _ in os.listdir("/Users/guiying/Downloads"):
+        if _[:4] == "萌娘百科" and _ [-15:] == "flowthread.json":
             try:
-                os.system('git add .')
-                os.system('git commit -m "Update"')
-                os.system('git push origin main')
-            except Exception as e:
-                print(e)
+                print(_)
+                os.rename("/Users/guiying/Downloads/" + _,
+                          "./Lih萌百镜像站flowthread-原.json")
+                for _ in json.load(open("Lih萌百镜像站flowthread-原.json")):
+                    newJSON[_["title"]] = _["posts"]
+                open("Lih萌百镜像站flowthread.json", "w").write(
+                    json.dumps(newJSON, indent=4))
             finally:
-                print("Fin")
+                try:
+                    os.system('git add .')
+                    os.system('git commit -m "Update"')
+                    os.system('git push origin main')
+                except Exception as e:
+                    print(e)
+                finally:
+                    print("——" * 10)
+                    sleep(3600)
