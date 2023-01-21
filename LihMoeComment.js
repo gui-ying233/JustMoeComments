c2ef92843efd8ab34bf4c545a12e31bb5c88bf35
gui-ying233 <zhuyufei20050307@gmail.com>
2023-01-20 21:38:22 -0500

    修复带有空格的页面不显示的问题；删除错误的selfLink逻辑；删除注释。

--- LihMoeComment.js
+++ LihMoeComment.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LihMoeComment
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  萌百看Lih的镜像站的评论
 // @author       鬼影233
 // @match        https://*.moegirl.org.cn/*
@@ -9,81 +9,72 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    fetch(
-	"https://raw.githubusercontent.com/gui-ying233/LihMoeFlowThreadSync/main/Lih萌百镜像站flowthread.json"
-)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		if (data[mw.config.get("wgPageName")]) {
-			const commentList = data[mw.config.get("wgPageName")].filter(
-				(d) => !d.status
-			);
-			const commentsSection = document.createElement("div");
-			commentsSection.id = "commentsSection";
-			commentsSection.style.cssText =
-				"display:flex;flex-wrap:wrap;justify-content:space-around;";
-			if (mw.config.get("skin") === "moeskin") {
-				commentsSection.style.cssText +=
-					"padding:.5em;border-bottom:1px dashed;background-color:var(--theme-background-color);line-height:1.75;";
-			}
-			switch (mw.config.get("skin")) {
-				case "vector":
-					document
-						.getElementById("content")
-						.appendChild(commentsSection);
-					break;
-				case "moeskin":
-				default:
-					document.body.getElementsByTagName("footer")[0].innerHTML =
-						commentsSection.outerHTML +
-						document.body.getElementsByTagName("footer")[0]
-							.innerHTML;
-					// .getElementById("moe-main-container")
-					// .appendChild(commentsSection);
-					break;
-			}
-			for (const c of commentList) {
-				const commentBox = document.createElement("div");
-				commentBox.id = "comment-" + c.id;
-				commentBox.classList.add("commentBox");
-				commentBox.innerHTML = `<div class="commentName"><a href="/U:${c.username}">${c.username}</a>：</div><div class="commentText">${c.text}</div>`;
-				if (c.parentid) {
-					document
-						.getElementById("comment-" + c.parentid)
-						.appendChild(commentBox);
-				} else {
-					document
-						.getElementById("commentsSection")
-						.appendChild(commentBox);
+(function () {
+	"use strict";
+	fetch(
+		"https://raw.githubusercontent.com/gui-ying233/LihMoeFlowThreadSync/main/Lih萌百镜像站flowthread.json"
+	)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			if (data[mw.config.get("wgPageName").replace("_", " ")]) {
+				const commentList = data[
+					mw.config.get("wgPageName").replace("_", " ")
+				].filter((d) => !d.status);
+				const commentsSection = document.createElement("div");
+				commentsSection.id = "commentsSection";
+				commentsSection.style.cssText =
+					"display:flex;flex-wrap:wrap;justify-content:space-around;";
+				if (mw.config.get("skin") === "moeskin") {
+					commentsSection.style.cssText +=
+						"padding:.5em;border-bottom:1px dashed;background-color:var(--theme-background-color);line-height:1.75;";
 				}
+				switch (mw.config.get("skin")) {
+					case "vector":
+						document
+							.getElementById("content")
+							.appendChild(commentsSection);
+						break;
+					case "moeskin":
+					default:
+						document.body.getElementsByTagName(
+							"footer"
+						)[0].innerHTML =
+							commentsSection.outerHTML +
+							document.body.getElementsByTagName("footer")[0]
+								.innerHTML;
+						break;
+				}
+				for (const c of commentList) {
+					const commentBox = document.createElement("div");
+					commentBox.id = "comment-" + c.id;
+					commentBox.classList.add("commentBox");
+					commentBox.innerHTML = `<div class="commentName"><a href="/U:${c.username}">${c.username}</a>：</div><div class="commentText">${c.text}</div>`;
+					if (c.parentid) {
+						document
+							.getElementById("comment-" + c.parentid)
+							.appendChild(commentBox);
+					} else {
+						document
+							.getElementById("commentsSection")
+							.appendChild(commentBox);
+					}
+				}
+				const commentImg = [
+					...document.body.querySelectorAll(
+						"#commentsSection img[src^='/images/']"
+					),
+				];
+				commentImg.map((i) => {
+					i.src =
+						"//img.moegirl.org.cn/common/" +
+						new URL(i.src).pathname.slice(8);
+				});
+				const commentCSS = document.createElement("style");
+				commentCSS.innerHTML =
+					".commentBox{flex:auto;margin:.25em;padding:.25em;border:1px solid;border-radius:.25em;}.commentName>a{border-bottom:1px dashed;}";
+				document.head.appendChild(commentCSS);
 			}
-			const commentSelfLink = [
-				...document.body.querySelectorAll(
-					"#commentsSection a.mw-selflink.selflink"
-				),
-			];
-			commentSelfLink.map((a) => {
-				a.classList.remove("mw-selflink", "selflink");
-				a.href = encodeURI("/U:鬼影233");
-			});
-			const commentImg = [
-				...document.body.querySelectorAll(
-					"#commentsSection img[src^='/images/']"
-				),
-			];
-			commentImg.map((i) => {
-				i.src =
-					"//img.moegirl.org.cn/common/" +
-					new URL(i.src).pathname.slice(8);
-			});
-			const commentCSS = document.createElement("style");
-			commentCSS.innerHTML =
-				".commentBox{flex:auto;margin:.25em;padding:.25em;border:1px solid;border-radius:.25em;}.commentName>a{border-bottom:1px dashed;}";
-			document.head.appendChild(commentCSS);
-		}
-	});
+		});
 })();
